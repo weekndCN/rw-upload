77c30a892c6e3a81d8d11d0b3fa84a40c7f11657
weeknd <weeknd.su@gmail.com>
2021-01-20 14:03:11 +0800

    remove local testing func

--- api/extra_test.go
+++ api/extra_test.go
@@ -1,13 +1,8 @@
 package api
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestUnzip(t *testing.T) {
-	err := Unzip("../temp/fic_data_screen.zip", "../uploads")
-	if err != nil {
-		fmt.Println(err)
-	}
 }
--- api/tree_test.go
+++ api/tree_test.go
@@ -1,15 +1,8 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
 	"testing"
 )
 
 func TestFileIterate(t *testing.T) {
-	// rootpath, _ := os.Getwd()
-	var rootFile *File
-	rootFile = FileIterate("rw-upload/temp")
-	output, _ := json.MarshalIndent(rootFile, "", "     ")
-	fmt.Println(string(output))
 }
